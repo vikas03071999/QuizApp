9e16937653e108d3ec1e2f52ce52c4230dbc66d1
Vikas Belal <belal.vikas.5@gmail.com>
2023-01-29 07:57:29 +0530

    Hosting spp on github pages

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <BrowserRouter>
+    <BrowserRouter basename="/QuizApp">
       <Routes>
         <Route exact path="/" element={<Intro />} />
         <Route path="/playGame" element={<App />} />
